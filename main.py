42b2199a9d670ad4b9b7337e989c678b0c760929
xaviguasch <xaviguasch@gmail.com>
2020-12-16 21:39:52 +0100

    added mail merge app

---
+++ main.py
@@ -0,0 +1,28 @@
+# TODO: Create a letter using starting_letter.docx
+
+PLACEHOLDER = "[name]"
+
+with open("./Input/Names/invited_names.txt", "r") as names_file:
+    names = names_file.readlines()
+
+with open("./input/Letters/starting_letter.docx") as letter_file:
+    letter_contents = letter_file.read()
+
+    for name in names:
+        stripped_name = name.strip()
+        new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
+        with open(f"./Output/ReadyToSend/letter_for_{stripped_name}.docx", "w") as completed_letter:
+            completed_letter.write(new_letter)
+
+
+
+
+
+# for each name in invited_names.txt
+# Replace the [name] placeholder with the actual name.
+# Save the letters in the folder "ReadyToSend".
+    
+# Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+    # Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+        # Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
